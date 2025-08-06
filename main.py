d6a7b1ae116f6a1980b5a13c17cad6e383ce6968
Rustam <rs_9_s9@yahoo.com>
2025-08-07 01:50:04 +0600

    Add back button and improve menu navigation, fixed a bag

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(f"Ошибка: {str(e)}", reply_markup=back_button_markup)
 
 # === Запуск ===
-if name == "__main__":
+if __name__ == "__main__":
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
